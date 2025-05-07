2959eabaff84f7477221518bbf8dd2d9bc3038c3
Shashindi Harischandra <shashindhieravishka@gmail.com>
2025-05-07 19:49:52 +0530

    Delete npx/Create backend using node.js & express.js directory

--- npx/Create backend using node.js & express.js/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require(' ./controller');
-
-router.get(' /users', controller.getUsers);
-router.post(' /createusers', controller.addUser);
-router.post(' /updateusers', controller.updateUser);
-router.post(' /deleteusers', controller.deletegetUser);
-
